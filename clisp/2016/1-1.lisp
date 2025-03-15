8633289e8964873f445aa28c817155639ae97a37
Kratuvid <ray779@tuta.io>
2025-03-15 23:55:37 +0530

    remove unnecessary cruft

--- clisp/2016/1-1.lisp
+++ clisp/2016/1-1.lisp
@@ -2,31 +2,29 @@
 
 	;; real deal brother
 	(progn
-	  (setq location '(0 0) direction 0)  ;; direction is represented like the hands of a clock, goes from 0 to 3
-	  (dolist (instruction-symbol (let ((input (make-array 0 :element-type 'character :adjustable t)))
-									(with-open-file (stream "../../inputs/2016/1")
-									  (adjust-array input (file-length stream))
-									  (read-sequence input stream))
-									(with-input-from-string
-										(input-stream (concatenate
-													   'string
-													   (substitute #\Space #\, (string-trim (string #\Newline) input))
-													   " $"))
-									  (read-delimited-list #\$ input-stream))
-									))
-		(let* ((instruction (string instruction-symbol))
-			   (direction-relative (elt instruction 0))
-			   (amount (parse-integer instruction :start 1)))
-		  (setq direction (mod (if (eql direction-relative #\L) (1- direction) (1+ direction)) 4))
-		  (setq displacement (mapcar #'* (list amount amount) (case direction
-																(0 '( 0  1))
-																(1 '( 1  0))
-																(2 '( 0 -1))
-																(3 '(-1  0)))))
-		  (setq location (mapcar #'+ location displacement))
-		  (format t "~%~a -> ~a, ~a -> ~a, ~a" direction-relative direction amount displacement location)))
-	  (format t "~%Total distance travelled was ~a" (reduce #'+ (mapcar #'abs location)))
-	  )
+	  (let ((location '(0 0)) (direction 0))  ;; direction is represented like the hands of a clock, goes from 0 to 3
+		(dolist (instruction-raw (let ((input (make-array 0 :element-type 'character :adjustable t)))
+								   (with-open-file (stream "../../inputs/2016/1"
+														   :direction :input
+														   :if-does-not-exist :error)
+									 (adjust-array input (file-length stream))
+									 (read-sequence input stream))
+								   (uiop:split-string (string-trim '(#\Newline #\Tab #\Space) input) :separator '(#\,))))
+		  (let* ((instruction (string-trim '(#\Space) instruction-raw))
+				 (direction-relative (char instruction 0))
+				 (amount (parse-integer instruction :start 1))
+				 (direction-new (mod (if (eql direction-relative #\L) (1- direction) (1+ direction)) 4))
+				 (displacement (case direction-new
+								 (0 (list 0 amount))
+								 (1 (list amount 0))
+								 (2 (list 0 (- amount)))
+								 (3 (list (- amount) 0)))))
+			(setq direction direction-new)
+			(setq location (mapcar #'+ location displacement))
+			(format t "~%~a ~a, ~a ~a, ~a" direction-relative amount direction displacement location)))
+		(format t "~%Total distance travelled was ~a" (reduce #'+ (mapcar #'abs location)))
+		))
+
 
 	;; experimentation and learning
 	(progn
