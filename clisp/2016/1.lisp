2e19144de98295334810b5d26372e5ed76554e2d
Kratuvid <ray779@tuta.io>
2025-03-16 07:05:07 +0530

    solved 2016/1.2!

--- clisp/2016/1.lisp
+++ clisp/2016/1.lisp
@@ -35,11 +35,10 @@
 	  ;; 	(format t "~%Total distance travelled was ~a" (reduce #'+ (mapcar #'abs final-location))))
 
 	  ;; Approach 2
-	  (let ((location '(0 0)) (direction 0))  ;; direction is represented like the hands of a clock, goes from 0 to 3
+	  (let ((location '(0 0)) (direction 0) ;; direction is represented like the hands of a clock, goes from 0 to 3
+			(visited-set '((0 0))) (first-visited-twice nil))
 		(dolist (instruction-raw (let ((input (make-array 0 :element-type 'character :adjustable t)))
-								   (with-open-file (stream "../../inputs/2016/1"
-														   :direction :input
-														   :if-does-not-exist :error)
+								   (with-open-file (stream "../../inputs/2016/1")
 									 (adjust-array input (file-length stream))
 									 (read-sequence input stream))
 								   (uiop:split-string (string-trim '(#\Newline #\Tab #\Space) input) :separator '(#\,))))
@@ -47,15 +46,36 @@
 				 (direction-relative (char instruction 0))
 				 (amount (parse-integer instruction :start 1))
 				 (direction-new (mod (if (eql direction-relative #\L) (1- direction) (1+ direction)) 4))
-				 (displacement (case direction-new
-								 (0 (list 0 amount))
-								 (1 (list amount 0))
-								 (2 (list 0 (- amount)))
-								 (3 (list (- amount) 0)))))
+				 (direction-vector (case direction-new
+									 (0 (list 0 1))
+									 (1 (list 1 0))
+									 (2 (list 0 -1))
+									 (3 (list -1 0))))
+				 (displacement (mapcar #'* (list amount amount) direction-vector)))
+			
+			(setq old-visited-set visited-set)
+			(dotimes (i amount)
+			  (let* ((step-displacement (mapcar #'* (list (1+ i) (1+ i)) direction-vector))
+					 (step-location (mapcar #'+ location step-displacement)))
+				(format t "~%: ~a, ~a, ~a" i step-displacement step-location)
+				(if (not first-visited-twice)
+					(if (member step-location visited-set :test #'equal)
+						(setq first-visited-twice step-location)
+						(setq visited-set (adjoin step-location visited-set :test #'equal)))
+					(return nil))))
+			(setq diff-visited-set (set-difference visited-set old-visited-set))
+			(if diff-visited-set (format t "~%# ~a" diff-visited-set))
+			
 			(setq direction direction-new)
 			(setq location (mapcar #'+ location displacement))
+			(if (not first-visited-twice) ;; if it isn't already populated
+				(if (member location visited-set) ;; we found our first twice visited location!
+					(setq first-visited-twice location) ;; save it
+					(adjoin location visited-set)))     ;; else insert it into the set
 			(format t "~%~a ~a, ~a ~a, ~a" direction-relative amount direction displacement location)))
 		(format t "~%Total distance travelled was ~a" (reduce #'+ (mapcar #'abs location)))
+		(format t "~%~a was the first twice visited location" first-visited-twice)
+		(format t "~%Distance to first location visited twice is ~a" (reduce #'+ (mapcar #'abs first-visited-twice)))
 		)
 
 	  )
