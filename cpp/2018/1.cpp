76a6be7aad39e2453c33e0605d10ff1c122d8ee5
Kratuvid <ray779@tuta.io>
2025-03-22 02:42:41 +0530

    tired of this crap

--- cpp/2018/1.cpp
+++ cpp/2018/1.cpp
@@ -1,29 +1,81 @@
+#include <charconv>
+#include <cstring>
+#include <sstream>
+#include <vector>
 #include "1.hpp"
 
-struct p1_result_t {
-	bool is_constant_evaluated;
-	int answer;
-};
+CONSTEVAL auto solver(std::string_view input) {
+	int sum = 0, twice_visited = std::numeric_limits<int>::min();
 
-consteval auto part_one(const auto& input) {
-	p1_result_t result {};
-	
-	for (char c : input) {
-		(void)c;
+	auto predicate = [](char x) {
+		return is_whitespace(x) || x == '+';
+	};
+	auto error = [&sum]() {
+		sum = std::numeric_limits<int>::min();
+	};
+
+	constexpr size_t visited_capacity = 8192;
+	size_t visited_size = 0;
+	std::array<int, visited_capacity> visited {};
+
+	while (input.size() > 0) {
+		input = trim(input, predicate);
+
+		int drift = 0;
+		auto status = std::from_chars(input.data(), input.data() + input.size(), drift);
+		if (status.ec != std::errc{}) { error(); break; }
+
+		sum += drift;
+		input = std::string_view(status.ptr, input.data() + input.size());
+
+		if (twice_visited == std::numeric_limits<int>::min()) {
+			if (visited_size >= visited_capacity) { error(); break; }
+
+			auto end = visited.begin() + visited_size + 1;
+			if (std::find(visited.begin(), end, sum) == end)
+				visited[visited_size++] = sum;
+			else
+				twice_visited = sum;
+		}
 	}
 
-	result.is_constant_evaluated = std::is_constant_evaluated();
-	return result;
+	using value_t = std::variant<int, std::string_view>;
+	return std::array<std::pair<std::string_view, value_t>, 3> {{
+		{"sum", sum}, {"twice_visited", twice_visited}
+	}};
 }
+
+auto solver2(std::string_view input) -> std::optional<int> {
+  std::istringstream iss(input.data());
+  std::vector<int> visited;
+  int current = 0;
+
+  while (iss) {
+    int drift = 0;
+    iss >> drift;
+    current += drift;
+
+    if (std::find(visited.begin(), visited.end(), current) == visited.end()) {
+      visited.push_back(current);
+    } else {
+      return current;
+    }
+  }
+
+  return std::nullopt;
+};
 
 int main() {
 	try {
-		constexpr auto p1_result = part_one(input);
-		std::println("Part one...");
-		std::println("Is constant evaluated: {}", p1_result.is_constant_evaluated);
-		std::println("Final answer: {}", p1_result.answer);
+		const auto solution {solver(g_input)};
+		print_results(solution);
+    std::println("Part 2: {}", *solver2(g_input));
 	}
 	catch (std::exception& e) {
-		std::println("Exception: {}", e.what());
+		std::println("Exception (std): {}", e.what());
+	} catch (const char*& e) {
+		std::println("Exception (char*): {}", e);
+	} catch (std::string_view& e) {
+		std::println("Exception (string view): {}", e);
 	}
 }
--- cpp/2018/1.hpp
+++ cpp/2018/1.hpp
@@ -4,9 +4,84 @@
 #include <fstream>
 #include <string_view>
 #include <type_traits>
+#include <functional>
 
-#define INPUT_FILENAME "../../inputs/2018/1"
+#define UU __attribute__((unused))
 
-constexpr std::string_view input {
-#include INPUT_FILENAME
+#define IS_CONSTEVAL 0
+#if IS_CONSTEVAL == 1
+#define CONSTEVAL consteval
+#else
+#define CONSTEVAL
+#endif
+
+using Log = std::array<char, 256>;
+
+constexpr std::string_view g_input {
+#include "../../inputs/2018/1"
 };
+
+template<>
+struct std::formatter<Log> {
+	template<class ParseContext>
+	constexpr auto parse(ParseContext& ctx) {
+		auto it = ctx.begin();
+		auto end = ctx.end();
+
+		if (it != end && *it != '}') {
+			throw std::format_error("Invalid format specifier for Log");
+		}
+
+		return it;
+	}
+
+	template<class FormatContext>
+	auto format(Log const& log, FormatContext& ctx) const {
+		return std::format_to(ctx.out(), "{}", log.data());
+	}
+};
+
+void print_results(auto const& results) {
+	for(auto const& item : results) {
+		std::print("{}: ", item.first);
+		std::visit([](auto const& value){std::println("{}", value);}, item.second);
+	}
+}
+
+template<class TPredicate>
+CONSTEVAL std::string_view trim_left(std::string_view const& str, TPredicate predicate) {
+	auto search = std::find_if_not(str.begin(), str.end(), predicate);
+	return std::string_view(search, str.end());
+}
+
+CONSTEVAL std::string_view trim_left(std::string_view const& str, char c) {
+	return trim_left(str, [c](char x){ return x == c; });
+}
+
+template<class TPredicate>
+CONSTEVAL auto trim_right(std::string_view const& str, TPredicate predicate) {
+	auto search = std::find_if_not(str.rbegin(), str.rend(), predicate);
+	return str.substr(0, std::distance(search, str.rend()));
+}
+
+CONSTEVAL auto trim_right(std::string_view const& str, char c) {
+	return trim_right(str, [c](char x){ return x == c; });
+}
+
+template<class TPredicate>
+CONSTEVAL auto trim(std::string_view const& str, TPredicate predicate) {
+	return trim_right(trim_left(str, predicate), predicate);
+}
+
+CONSTEVAL auto trim(std::string_view const& str, char c) {
+	return trim_right(trim_left(str, c), c);
+}
+
+CONSTEVAL bool is_whitespace(char x) {
+	static constexpr std::string_view ws {" \f\n\r\t\v"};
+	return ws.find(x) != std::string_view::npos;
+}
+
+CONSTEVAL auto trim_whitespaces(std::string_view const& str) {
+	return trim(str, is_whitespace);
+}
